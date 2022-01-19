3094e6000dde584b4373de8160e0fe3a7d3bddeb
jhk <mallangyi@naver.com>
2022-01-19 23:29:44 +0900

    testFile Create Test

---
+++ src/main/java/ChangeMachine.java
@@ -0,0 +1,15 @@
+public class ChangeMachine {
+
+    private int money = 0;
+
+    public ChangeMachine(int money) {
+        this.money = money ;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+
+
+}
--- src/test/java/TestChangeMachine.java
+++ src/test/java/TestChangeMachine.java
@@ -1,9 +1,13 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestChangeMachine {
 
     @Test
     public void testGetChangeAmount() {
-
+        ChangeMachine cm = new ChangeMachine(1000);
+        cm.getMoney();
+        Assert.assertEquals(1000, cm.getMoney() );
     }
+
 }
