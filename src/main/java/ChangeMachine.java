2b351ae5963731f67574a5f595371d97d1039357
jhk <mallangyi@naver.com>
2022-01-23 20:38:44 +0900

    내가 파악해서 만든 테스트 케이스

--- src/main/java/ChangeMachine.java
+++ src/main/java/ChangeMachine.java
@@ -4,11 +4,16 @@ public class ChangeMachine {
 
     private int drinkPrice = 0;
 
-    public ChangeMachine(int money) {
-        this.money = money ;
+    public ChangeMachine(){
     }
 
-    public ChangeMachine(){
+    public ChangeMachine(int money) {
+        this.money = money;
+    }
+
+    public ChangeMachine(int money, int drink) {
+        this.money = money;
+        this.drinkPrice = drink;
     }
 
     public int getMoney() {
@@ -25,5 +30,28 @@ public class ChangeMachine {
 
     public int getDrinkPrice() {
         return this.drinkPrice;
+    }
+
+    public int getChange() {
+        int money = this.money;
+        int drinkPrice = this.drinkPrice;
+
+        int fiveH, oneH, fiveT, T ;
+        int temp = money - drinkPrice ;
+
+        int[] changeArr = {500, 100, 50, 10};
+        int[] changeCnt = new int[4];
+
+        int loop = 0;
+        for(int change : changeArr ) {
+            if (temp / change > 0) {
+                changeCnt[loop] = temp / change;
+                temp = temp % change;
+            }
+
+            loop = loop + 1;
+        }
+
+        return changeCnt[1];
     }
 }
--- src/test/java/TestChangeMachine.java
+++ src/test/java/TestChangeMachine.java
@@ -19,10 +19,25 @@ public class TestChangeMachine {
     }
 
     @Test
-    public void TestSetDrinkPrice_칠백원_어치_음료수_돈확인() {
+    public void TestSetDrinkPrice_천원_어치_음료수_돈확인() {
         ChangeMachine cm = new ChangeMachine();
         cm.setDrinkPrice(1000);
         assertThat(cm.getDrinkPrice(),is(1000));
+    }
+
+    @Test
+    public void TestGetChange_천원넣고_칠백원_음료수_샀을때_거스름돈_확인(){
+
+        //given
+        int money = 1000;
+        int drink = 300;
+        ChangeMachine cm = new ChangeMachine(money,drink);
+
+        //when
+        int change = cm.getChange();
+
+        //then
+        assertThat(change, is(2));
     }
 
 }
